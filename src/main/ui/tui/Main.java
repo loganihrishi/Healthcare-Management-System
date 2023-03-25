83388321232645a4080784c47e85b77cec4a9933
hrishilogani <hlogani@student.ubc.ca>
2023-03-25 16:02:43 -0700

    Added exception handling

--- src/main/ui/tui/Main.java
+++ src/main/ui/tui/Main.java
@@ -1,14 +1,19 @@
 package ui.tui;
 
+import javax.imageio.IIOException;
 import java.io.IOException;
 import java.text.ParseException;
+
 /**
  * This is the main class of the project. It is used to run the application and start its user interface.
  */
 
 public class Main {
-    public static void main(String[] args) throws ParseException, IOException {
-        StartApplication st = new StartApplication();
-        st.displayMenu();
+    public static void main(String[] args) {
+        try {
+            new StartApplication();
+        } catch (Exception e) {
+            System.err.println("Error running the application");
+        }
     }
 }
